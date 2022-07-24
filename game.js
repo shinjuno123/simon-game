f0fd275d70a8e0435745c1a9c6fd97113e6352da
shinjuno123 <shinjuno123@naver.com>
2022-07-24 22:38:45 +0900

    feat: Add starting a game and going to next stage and restarting a game

--- game.js
+++ game.js
@@ -1,19 +1,23 @@
-var userChosenColour = []
+var levelTitle = $("#level-title");
+var body = $("body");
 var buttonColours = ["red", "blue", "green", "yellow"];
+var randomChosenColour =''
 var currentState = [];
+var userChosenColour = []
+var isGameStarted = false;
 
-function nextSquence(){
+function chooseRandomColor(){
   var randomNumber = Math.floor(Math.random() * 4);
   return randomNumber;
 }
 
 
-function buttonFadeInAndOut(gamePattern){
-  $("."+gamePattern).fadeOut(100).fadeIn(100);
+function buttonFadeInAndOut(randomChosenColour){
+  $("."+randomChosenColour).fadeOut(100).fadeIn(100);
 }
 
-function makeSound(gamePattern){
-  var sound = new Audio("./sounds/"+gamePattern+".mp3");
+function makeButtonSound(randomChosenColour){
+  var sound = new Audio("./sounds/"+randomChosenColour+".mp3");
   sound.play();
 }
 
@@ -24,21 +28,74 @@ function animatePress(clickedButton){
   setTimeout(function(){button.removeClass("pressed")},100)
 }
 
+function changeLevelHeading(randomChosenColour){
+  levelTitle.text("Level " + currentState.length);
+}
 
 
-$(this).on("keydown",function(event){
-  var randomChosenColour = buttonColours[nextSquence()];
-  var gamePattern = randomChosenColour;
+function isSameColor(){
+  if(userChosenColour[userChosenColour.length - 1] === currentState[userChosenColour.length - 1]){
+    return true
+  }else{
+    return false
+  }
+}
+
+
+function goToNextLevel(){
+  randomChosenColour = buttonColours[chooseRandomColor()];
   currentState.push(randomChosenColour);
+  changeLevelHeading(randomChosenColour);
 
   userChosenColour = [];
 
   setTimeout(function(){
-    buttonFadeInAndOut(gamePattern);
-    makeSound(gamePattern);
+    buttonFadeInAndOut(randomChosenColour);
+    makeButtonSound(randomChosenColour);
   },500);
+}
 
 
+function startOver(){
+  randomChosenColour = buttonColours[chooseRandomColor()];
+  currentState.push(randomChosenColour);
+  changeLevelHeading(randomChosenColour);
+
+  userChosenColour = [];
+
+  setTimeout(function(){
+    buttonFadeInAndOut(randomChosenColour);
+    makeButtonSound(randomChosenColour);
+  },500);
+}
+
+
+function gameOver(){
+  levelTitle.text("Game Over, Press Any Key to Restart");
+  body.addClass('game-over');
+  setTimeout(function(){body.removeClass('game-over');},200);
+
+  // play a sound that happens when the user's color is wrong in sequence.
+  var wrong = new Audio("./sounds/wrong.mp3");
+  wrong.play();
+}
+
+// initialize a game when games over.
+function initialize(){
+  userChosenColour = [];
+  currentState = [];
+
+  isGameStarted = false;
+}
+
+
+
+
+$(document).on("keydown",function(event){
+  if(isGameStarted === false){
+    startOver();
+    isGameStarted = true;
+  }
 });
 
 
@@ -46,6 +103,15 @@ $(".btn").click(function(){
   var clickedButton = this.classList[1];
   userChosenColour.push(clickedButton);
   animatePress(clickedButton);
-  makeSound(clickedButton);
-  console.log(userChosenColour);
+  makeButtonSound(clickedButton);
+  if(isSameColor()){
+    // if all the colors user chose was correct then go to next level.
+    if(userChosenColour.length === currentState.length){
+      goToNextLevel();
+    }
+
+  }else{
+    gameOver();
+    initialize();
+  }
 });
